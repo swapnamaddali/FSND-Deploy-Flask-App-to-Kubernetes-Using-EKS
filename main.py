1077c1f091e27ddfa9933083a464128ff96daf8f
swapna Maddali <sc986c@att.com>
2020-01-17 18:39:22 -0500

    test changes for CI/CD pipeline latest

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Very Healthy")
+    return jsonify("Very Very Healthy and good")
 
 
 @APP.route('/auth', methods=['POST'])
